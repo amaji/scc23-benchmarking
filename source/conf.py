25f02c2dd923719b5d137190460b1be9bf470a5c
Amiya Maji <amaji@purdue.edu>
2023-10-13 18:27:10 -0400

    Enable autosectionlabel

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,10 @@ rst_epilog += '.. |project_lower| replace:: %s\n' % project_lower
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autosectionlabel"]
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
